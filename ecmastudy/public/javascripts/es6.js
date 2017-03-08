86e1d8e5c76597e4c95483a99efe553306e19886
yunikoro <542031180@qq.com>
2017-03-09 00:03:13 +0800

    guess number add listener

--- ecmastudy/public/javascripts/es6.js
+++ ecmastudy/public/javascripts/es6.js
@@ -2,3 +2,23 @@
  *  @yunikoro
  */
 console.log('hello ECMA 2017');
+
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+var guesses = document.querySelector('.guesses');
+var lastResult = document.querySelector('.lastResult');
+var lowOrHi = document.querySelector('.lowOrHi');
+
+var guessSubmit = document.querySelector('.guessSubmit');
+var guessField = document.querySelector('.guessField');
+
+var guessCount = 1;
+var resetButton;
+
+
+function checkGuess() {
+	a = 1;
+	b = 1;
+	c = '1';
+	alert('I am a placeholder');
+}

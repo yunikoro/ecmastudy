2e71e46ff930c13ab25e99b38ca4742bb87b19c8
yunikoro <542031180@qq.com>
2017-03-15 23:39:10 +0800

    Active learning: sizing a canvas box

--- ecmastudy/public/javascripts/es6.js
+++ ecmastudy/public/javascripts/es6.js
@@ -45,4 +45,33 @@ function checkGuess() {
 	guessField.focus();
 }
 
+function setGameOver() {
+	guessField.disabled = true;
+	guessSubmit.disabled = true;
+	resetButton = document.createElement('button');
+	resetButton.textContent = 'Start new game';
+	document.body.appendChild(resetButton);
+	resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+	guessCount = 1;
+	
+	var resetParas = document.querySelectorAll('.resultParas p');
+	for(var i = 0; i < resetParas.length; i++) {
+		resetParas[i].textContent = '';
+	}
+	resetButton.parentNode.removeChild(resetButton);
+	
+	guessField.disabled = false;
+	guessSubmit.disabled = false;
+	guessField.value = '';
+	guessField.focus();
+	
+	lastResult.style.backgroundColor = 'white';
+	
+	randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
 guessSubmit.addEventListener('click', checkGuess);
+
